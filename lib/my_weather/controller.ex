f1eff7779c238bc299fbec434f4bbf9179a53c57
thang.pham <thang.pham@cogo.co>
2022-09-07 14:21:09 +1200

    make controller run more thin and self-documenting

--- lib/my_weather/controller.ex
+++ lib/my_weather/controller.ex
@@ -13,40 +13,36 @@ defmodule MyWeather.Controller do
   end
 
   def run() do
-    ui = ui_client()
-
-    @location
-    |> provider().current_weather()
-    |> case do
-      {:ok, data} -> ui.display_weather(data)
-      _ -> ui.display_error()
-    end
-
+    fetch_and_display_weather()
     reschedule()
+    :ok = wait_for_next_poll()
+    run()
+  end
 
+  defp wait_for_next_poll() do
     receive do
       :exit -> Process.exit(self(), :normal)
-      :fetch_weather -> run()
+      :fetch_weather -> :ok
     end
   end
 
-  defp provider() do
-    app_config() |> Keyword.get(:provider)
+  defp fetch_and_display_weather() do
+    ui_client = config_for(:ui_client)
+    weather_provider = config_for(:provider)
+
+    @location
+    |> weather_provider.current_weather()
+    |> case do
+      {:ok, data} -> ui_client.display_weather(data)
+      _ -> ui_client.display_error()
+    end
   end
 
-  defp ui_client() do
-    app_config() |> Keyword.get(:ui_client)
-  end
-
-  defp interval() do
-    app_config() |> Keyword.get(:interval, @interval)
-  end
-
-  defp app_config() do
-    Application.get_env(:my_weather, MyWeather.Controller, [])
+  defp config_for(key, default \\ nil) do
+    Application.get_env(:my_weather, MyWeather.Controller, []) |> Keyword.get(key, default)
   end
 
   defp reschedule() do
-    Process.send_after(self(), :fetch_weather, interval())
+    Process.send_after(self(), :fetch_weather, config_for(:interval, @interval))
   end
 end
